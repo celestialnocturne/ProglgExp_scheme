594966ca9e163b308169aa1377b2fc800edfe7fd
Yuichiro KIMIJIMA <k1710199@edu.cc.uec.ac.jp>
2019-07-22 14:23:49 +0900

    ex2-2 has done.

--- ex2.scm
+++ ex2.scm
@@ -15,3 +15,25 @@
           )))
 
 ;ex2-2
+(define search
+  (lambda (list name dpt)
+    (cond ((equal? name (car list)) dpt)
+          ((null? (cdr list)) 0)
+          (else  (apply + (map (lambda (t) (search t name (+ dpt 1))) (cdr list))))
+          )))
+
+(define get-cousin
+  (lambda (list name)
+    (get-depth list (search list name 0))
+    ))
+
+
+;ex2-3
+(define get-path
+  (lambda (list name)
+    (cond ((equal? name (car list)) `(,name))
+          ((< 1 (search (cdr list) name 0)) `(,(car list)))
+          ((null? (cdr list)) '() )
+          
+          (else  (map (lambda (t) (get-path t name)) (cdr list)))
+          )))
